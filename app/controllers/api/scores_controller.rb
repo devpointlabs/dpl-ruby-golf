27aa8620fdbded4761754758b5c045934fcd4f4e
Jake Sorce <jakesorce@gmail.com>
2017-10-23 13:02:29 -0600

    holes completed logic

--- app/controllers/api/scores_controller.rb
+++ app/controllers/api/scores_controller.rb
@@ -5,11 +5,15 @@ class Api::ScoresController < ApplicationController
 
     User.all.each do |user|
       user.scores.each do |score|
-        total = score['total']
         complete = score['complete']
-        date = score['date']
-        github_url = score['github_url']
-        result =  { email: user.email, date: date, score: total, github_url: github_url, complete: complete }
+        result =  {
+          email: user.email,
+          date: score['date'],
+          score: score['total'],
+          github_url: score['github_url'],
+          complete: complete,
+          holes_completed: score['holes_completed']
+        }
         if complete
           complete_scores << result
         else
@@ -22,9 +26,14 @@ class Api::ScoresController < ApplicationController
 
   def create
     complete = true
+    holes_completed = 0
     scores = params[:scores].values.map do |value|
       parsed = value.to_i
-      complete = false if parsed == 0
+      if parsed > 0
+        holes_completed += 1
+      else
+        complete = false
+      end
       parsed
     end
     total = scores.inject(&:+)
@@ -33,7 +42,8 @@ class Api::ScoresController < ApplicationController
       scores: scores,
       total: total,
       github_url: params[:github_url],
-      complete: complete
+      complete: complete,
+      holes_completed: holes_completed
     }
     current_user.save
     render json: current_user
